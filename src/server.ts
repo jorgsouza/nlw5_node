f7a8b6516aa71c1f13e6d59c6c0cdc7858213db2
Jorge Souza <jorge.souza@ilumeo.com.br>
2021-04-21 23:52:51 -0300

    feat: First Commit

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express, { response } from "express";
+import "./database";
+import { routes } from "./routes";
+
+const port = 3333;
+
+const app = express();
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
