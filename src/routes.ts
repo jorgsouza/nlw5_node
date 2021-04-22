92676aa2331ac06af5502fc42e892359953ee108
Jorge Souza <jorge@vieirasouza.com.br>
2021-04-22 00:29:44 -0300

    feat: Add src/controllers and routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,22 +1,9 @@
 import { Router } from "express";
-import { getCustomRepository } from "typeorm";
-import { SettingsRepository } from "./repositories/SettingsRepository";
-
+import { SettingsController } from "./controllers/SettingsController";
 const routes = Router();
 
-routes.post("/settings", async (req, res) => {
-  const { chat, username } = req.body;
+const settingsController = new SettingsController();
 
-  const settingsRepository = getCustomRepository(SettingsRepository);
-
-  const settings = settingsRepository.create({
-    chat,
-    username,
-  });
-
-  await settingsRepository.save(settings);
-
-  return res.json(settings);
-});
+routes.post("/settings", async (request, response) => {});
 
 export { routes };
