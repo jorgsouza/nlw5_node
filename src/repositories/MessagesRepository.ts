2f7c1037291b9b0e1cb4444de76082dec51ca0da
Jorge Souza <jorge@vieirasouza.com.br>
2021-04-24 16:10:44 -0300

    feat: add new repo/entity/controllers and folder public

--- src/repositories/MessagesRepository.ts
+++ src/repositories/MessagesRepository.ts
@@ -1,4 +1,4 @@
-import { Entity, EntityRepository, Repository } from "typeorm";
+import { EntityRepository, Repository } from "typeorm";
 import { Message } from "../entities/Message";
 
 @EntityRepository(Message)
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,6 @@
-import "reflect-metadata";
-import express from "express";
+import { http } from "./http";
+import "./websocket/client";
 
-import { routes } from "./routes";
+const port = 3333;
 
-import "./database";
-// const port = 3333
-const app = express();
-
-app.use(express.json());
-app.use(routes);
-
-app.listen(3333, () => console.log("Server started on 3333"));
+http.listen(port, () => console.log(`Server started on ${port}`));
