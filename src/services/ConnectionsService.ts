5f684778a765982920f2136fde77a22f13e54e25
Jorge Souza <jorge@vieirasouza.com.br>
2021-04-25 15:59:28 -0300

    all

--- src/services/ConnectionsService.ts
+++ src/services/ConnectionsService.ts
@@ -1,4 +1,6 @@
 import { getCustomRepository } from "typeorm";
+import { Connection } from "../entities/Connection";
+
 import { ConnectionsRepository } from "../repositories/ConnectionsRepository";
 
 interface IConnectionCreate {
@@ -24,16 +26,40 @@ class ConnectionsService {
     });
 
     await this.connectionsRepository.save(connection);
+  }
+
+  async findByUserId(user_id: string) {
+    const connection = this.connectionsRepository.findOne({ user_id });
 
     return connection;
   }
 
-  async findByUserId(user_id: string) {
-    const connection = await this.connectionsRepository.findOne({
-      user_id,
+  async findAllWithoutAdmin() {
+    const connections = this.connectionsRepository.find({
+      where: {
+        admin_id: null,
+      },
+      relations: ["user"],
     });
 
+    return connections;
+  }
+
+  async findBySocketID(socket_id: string) {
+    const connection = this.connectionsRepository.findOne({ socket_id });
+
     return connection;
+  }
+
+  async updateAdminID(user_id: string, admin_id: string) {
+    await this.connectionsRepository
+      .createQueryBuilder()
+      .update(Connection)
+      .set({ admin_id })
+      .where("user_id = :user_id", {
+        user_id,
+      })
+      .execute();
   }
 }
 
